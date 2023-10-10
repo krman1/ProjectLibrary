bee97a665d3a030ac1af1cca16c62e9245dd0759
krman1 <mar.aniol@interia.pl>
2023-10-10 21:58:59 +0200

    10.10

---
+++ ProjectLibrary/ProjectLibrary/Reposirories/GenericProjectRepositories.cs
@@ -0,0 +1,25 @@
+﻿using ProjectLibrary.Entities;
+
+namespace ProjectLibrary.Reposirories
+{
+    public class GenericProjectRepositories<T>
+    {
+        private readonly List<T> _items = new();
+        public void Add(T item) 
+        {
+            item.Id = _items.Count + 1;
+            _items.Add(item);
+        }
+        public void Save()
+        {
+            foreach (var item in _items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public Project GetById(int id) 
+        {
+            return _items.Single(item =>item.Id == id);
+        }
+    }
+}
