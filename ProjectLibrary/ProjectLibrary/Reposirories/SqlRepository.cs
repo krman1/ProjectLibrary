0b93743cc635a98cad7f2f83a8ada93dfbdcded1
krman1 <mar.aniol@interia.pl>
2023-10-26 23:12:11 +0200

    26.10

--- ProjectLibrary/ProjectLibrary/Reposirories/SqlRepository.cs
+++ ProjectLibrary/ProjectLibrary/Reposirories/SqlRepository.cs
@@ -4,25 +4,30 @@ using ProjectLibrary.Entities;
 
 namespace ProjectLibrary.Reposirories
 {
-    public class SqlRepository
+    public class SqlRepository<T> where T: class, IEntity, new() 
     {
-        private readonly DbSet<Project> _dbSet;
         private readonly DbContext _dbContext;
+        private readonly DbSet<T> _dbSet;
 
         public SqlRepository(DbContext dbContext)
         {
             _dbContext = dbContext;
-            _dbSet = _dbContext.Set<Project>();
+            _dbSet = _dbContext.Set<T>();
         }
-        public Project? GetById(int id)
+        public IEnumerable<T> GetAll()
+        {
+            return _dbSet.OrderBy(item => item.Id).ToList();
+        }
+        
+        public T GetById(int id)
         {
             return _dbSet.Find(id);
         }
-        public void Add(Project item) 
+        public void Add(T item) 
         { 
             _dbSet.Add(item);
         }
-        public void Remove(Project item) 
+        public void Remove(T item) 
         { 
             _dbSet.Remove(item);
         }
