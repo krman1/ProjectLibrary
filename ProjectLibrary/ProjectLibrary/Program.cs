d63b860f146ccfca10493ebec2c6a8e51bf47445
krman1 <mar.aniol@interia.pl>
2023-11-01 23:08:48 +0100

    01.11

--- ProjectLibrary/ProjectLibrary/Program.cs
+++ ProjectLibrary/ProjectLibrary/Program.cs
@@ -3,13 +3,13 @@ using ProjectLibrary.Entities;
 using ProjectLibrary.Reposirories;
 
 /*var projectRepositories = new GenericProjectRepositories<Project>();
-projectRepositories.Add(new ProjectLibrary.Entities.Project { Name = "Chawelańska", City = "Poznań", Architect = "Minkiewicz", Description = "Budynek wielorodzinny" });
-projectRepositories.Add(new ProjectLibrary.Entities.Project { Name = "Grabiszyńska", City = "Łódź", Architect = "Kowalski", Description = "Hotel" });
+projectRepositories.Add(new Project { Name = "Chawelańska", City = "Poznań", Architect = "Minkiewicz", Description = "Budynek wielorodzinny" });
+projectRepositories.Add(new Project { Name = "Grabiszyńska", City = "Łódź", Architect = "Kowalski", Description = "Hotel" });
 projectRepositories.Save();*/
 
-var sqlRepository = new SqlRepository(new ProjectLibraryDbContext());
+var sqlRepository = new SqlRepository<Project>(new ProjectLibraryDbContext());
 sqlRepository.Add(new ProjectLibrary.Entities.Project { Name = "Chawelańska", City = "Poznań", Architect = "Minkiewicz", Description = "Budynek wielorodzinny" });
 sqlRepository.Add(new ProjectLibrary.Entities.Project { Name = "Grabiszyńska", City = "Łódź", Architect = "Kowalski", Description = "Hotel" });
 sqlRepository.Save();
-var pro = sqlRepository.GetById(1);
+var pro = sqlRepository.GetById(2);
 Console.WriteLine(pro.ToString());
